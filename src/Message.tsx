efe4530c6b867bab148f4bcc80dabce873ff00a7
Just Emmy <87806620+Emmyu@users.noreply.github.com>
2024-04-02 09:35:38 +0100

    second commit

--- src/Message.tsx
+++ src/Message.tsx
@@ -1,6 +1,8 @@
 function Message   () {
 }
 const name = 'Emmy'
+if (name)
+return <h1>Hello {name} </h1>;
 return <h1>Hello world</h1>;
 
 export default Message;
--- src/components/ListGroup.tsx
+++ src/components/ListGroup.tsx
@@ -1,7 +1,15 @@
 
 function ListGroup () {
 
-    return <h1>ListGroup</h1>
+    return (
+   <ul className="list-group">
+    <li className="list-group-item">An item</li>
+    <li className="list-group-item">A second item</li>
+    <li className="list-group-item">A third item</li>
+    <li className="list-group-item">A fourth item</li>
+    <li className="list-group-item">And a fifth one</li>
+  </ul>
+  );
 }
 
 
